cd4c7e42e01639a6b326c35a31d1932b97eb4697
Mitu Adrian <adrianmitu83@gmail.com>
2024-11-17 12:40:28 +0200

    Added Percolation constructor

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -1,10 +1,22 @@
 public class Percolation {
     int gridSize;
+    int[][] grid;
+    WeightedQuickUnionPathCompressionUF UnionFindStructure;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("Constructor cannot take a negative value like: " + n);
+        }
+
+        this.gridSize = n;
+        this.grid = new int[n][n];
+        this.UnionFindStructure = new WeightedQuickUnionPathCompressionUF(n);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                grid[i][j] = 0;
+            }
         }
     }
 
