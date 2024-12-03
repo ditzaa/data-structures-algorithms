fb5c4066a871482325660752d4bf138e74971c04
Mitu Adrian <adrianmitu83@gmail.com>
2024-12-03 09:37:27 +0200

    Commit before sorting refactor

--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -8,7 +8,6 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.Arrays;
 import java.util.Comparator;
 
 public class FastCollinearPoints {
@@ -52,11 +51,12 @@ public class FastCollinearPoints {
                 }
             }
             // Sort the points according to the slopes they makes with p
+
             Comparator<Point> slopeComparator = points[i].slopeOrder();
-            Arrays.sort(comparedPoints, slopeComparator);
-            for (int j = 0; j < pointsLength - 2; j++) {
-                slopeComparator.compare(comparedPoints[j], comparedPoints[j + 1]);
-            }
+            // private void sort(Point[] points, int[] slopeOfPoints, int lo, int hi) {
+            sort(comparedPoints, slopesOfComparedPoints, 0, pointsLength - 1);
+            
+
             // 3 (or more) adjacent points are collinear if they have the same slope
             int nbOfPointsWithSameSlope = 0;
             int indexFirstPointOfLine = -1;
@@ -80,6 +80,9 @@ public class FastCollinearPoints {
             if (areCollinearPoints) {
                 LineSegment newLineSegment = new LineSegment(comparedPoints[indexFirstPointOfLine],
                                                              comparedPoints[indexLastPointOfLine]);
+                if (numberOfSegments <= lineSegments.length) {
+                    resizeLineSegmentsArray();
+                }
                 lineSegments[numberOfSegments++] = newLineSegment;
             }
         }
@@ -107,6 +110,66 @@ public class FastCollinearPoints {
             newArray[i] = lineSegments[i];
         }
         lineSegments = newArray;
+    }
+
+    private void merge(Point[] points, double[] slopeOfPoints, int lo, int mid, int hi) {
+        // length of temp arrays
+        int n1 = mid - hi + 1;
+        int n2 = hi - 1;
+
+        // create temp arrays for slope sizes
+        Point[] subPointsArray1 = new Point[n1];
+        Point[] subPointsArray2 = new Point[n2];
+        // create temp arrays for slope sizes
+        int subSlopeArray1[] = new int[n1];
+        int subSlopeArray2[] = new int[n2];
+
+        // initial indices of first and second subarrays
+        int i = 0, j = 0;
+
+        int k = lo;
+        while (i < n1 && j < n2) {
+            if (slopeOfPoints[i] <= slopeOfPoints[j]) {
+                points[k] = subPointsArray1[i];
+                slopeOfPoints[k] = subSlopeArray1[i];
+                i++;
+            }
+            else {
+                points[k] = subPointsArray1[j];
+                slopeOfPoints[k] = subSlopeArray1[j];
+                j++;
+            }
+            k++;
+
+            // copy remaining elements of left subbarrays if any
+            while (i < n1) {
+                points[k] = subPointsArray1[i];
+                slopeOfPoints[k] = subSlopeArray1[i];
+                i++;
+                k++;
+            }
+
+            // copy remaining elements of right subbarrays if any
+            while (j < n2) {
+                points[k] = subPointsArray1[j];
+                slopeOfPoints[k] = subSlopeArray1[j];
+                j++;
+                k++;
+            }
+        }
+    }
+
+    private void sort(Point[] points, double[] slopeOfPoints, int lo, int hi) {
+        if (lo < hi) {
+            int mid = lo + (hi - 1) / 2;
+
+            sort(points, slopeOfPoints, lo, mid);
+            sort(points, slopeOfPoints, mid, hi);
+
+            merge(points, slopeOfPoints, lo, mid, hi);
+
+        }
+
     }
 
     public static void main(String[] args) {
