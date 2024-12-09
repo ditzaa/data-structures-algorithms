b657bb84924ca0c5659732dbb40c1c2658624c21
Mitu Adrian <adrianmitu83@gmail.com>
2024-12-09 17:25:05 +0200

    Implemented initial version of Board API

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -4,47 +4,163 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Board {
+    private int boardSize;
+    private int[][] tiles;
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
+        this.boardSize = tiles.length;
+        this.tiles = new int[boardSize][boardSize];
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                this.tiles[i][j] = tiles[i][j];
+            }
+        }
 
     }
 
     // string representation of this board
     public String toString() {
-        return null;
+        StringBuilder s = new StringBuilder();
+        s.append(boardSize + "\n");
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                s.append(String.format("%2d ", tiles[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
     }
 
     // board dimension n
     public int dimension() {
-        return 0;
+        return boardSize;
     }
 
     // number of tiles out of place
     public int hamming() {
-        return 0;
+        int distance = 0;
+        int goalNumber = 1;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (tiles[i][j] != goalNumber && tiles[i][j] != 0) {
+                    distance++;
+                }
+                goalNumber++;
+            }
+
+        }
+        return distance;
     }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
-        return 0;
+        // for each site, get its goal number, calculate the distance
+        // and add it to the distance
+        int distance = 0;
+        int goalNumber = 1;
+        int row = -1;
+        int col = -1;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                // don't count the last goal element, it should be the blank tile
+                if (i == boardSize - 1) {
+                    if (j == boardSize - 1) {
+                        return distance;
+                    }
+                }
+                // find the coordiantes of the goal number in our tiles array
+                row = findRowTile(goalNumber);
+                col = findColTile(goalNumber);
+                // calculate nb of moves to replace the current tile with the goal one
+                int newDistance = Math.abs(i - row) + Math.abs(j - col);
+                // StdOut.println(goalNumber + " " + newDistance + " (" + i + "," + row + "; " +
+                //                        j + "," + col + ")");
+                distance += newDistance;
+                goalNumber++;
+            }
+        }
+
+        return distance;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-        return false;
+        int distance = 0;
+        int goalNumber = 1;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (tiles[i][j] != goalNumber && tiles[i][j] != 0) {
+                    return false;
+                }
+                goalNumber++;
+            }
+        }
+
+        // last tile is blank tile (==0)
+        if (tiles[boardSize - 1][boardSize - 1] != 0) {
+            return false;
+        }
+
+        return true;
     }
 
     // does this board equal y?
     public boolean equals(Object y) {
-        return false;
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
+        Board that = (Board) y;
+        if (this.boardSize != that.boardSize) return false;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (this.tiles[i][j] != that.tiles[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null;
+        Queue<Board> boardQueue = new Queue<>();
+        // coordinates of blank tile
+        int row = findRowTile(0);
+        int col = findColTile(0);
+
+        Board newBoard;
+        // upper neighbour
+        if (row - 1 >= 0) {
+            newBoard = this.copy();
+            exchangeTiles(newBoard.tiles, row, col, row - 1, col);
+            boardQueue.enqueue(newBoard);
+        }
+        // righter neighbour
+        if (col + 1 < boardSize) {
+            newBoard = this.copy();
+            exchangeTiles(newBoard.tiles, row, col, row, col + 1);
+            boardQueue.enqueue(newBoard);
+        }
+        // below neighbour
+        if (row + 1 < boardSize) {
+            newBoard = this.copy();
+            exchangeTiles(newBoard.tiles, row, col, row + 1, col);
+            boardQueue.enqueue(newBoard);
+        }
+        // below neighbour
+        if (col - 1 >= 0) {
+            newBoard = this.copy();
+            exchangeTiles(newBoard.tiles, row, col, row, col - 1);
+            boardQueue.enqueue(newBoard);
+        }
+        return boardQueue;
     }
 
     // a board that is obtained by exchanging any pair of tiles
@@ -52,9 +168,91 @@ public class Board {
         return null;
     }
 
+    private int findRowTile(int number) {
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (tiles[i][j] == number) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private int findColTile(int number) {
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (tiles[i][j] == number) {
+                    return j;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private Board copy() {
+        int boardSize = this.dimension();
+        int[][] copiedTiles = new int[boardSize][boardSize];
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                copiedTiles[i][j] = this.tiles[i][j];
+            }
+        }
+        Board newBoard = new Board(copiedTiles);
+        return newBoard;
+    }
+
+    // exchange values between tile0 and tile1
+    private void exchangeTiles(int[][] tiles, int row0, int col0, int row1, int col1) {
+        int aux = tiles[row0][col0];
+        tiles[row0][col0] = tiles[row1][col1];
+        tiles[row1][col1] = aux;
+    }
+
     // unit testing (not graded)
     public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+        StdOut.println(initial.toString());
+        // StdOut.println("Hamming: " + initial.hamming());
+        // StdOut.println("Manhattan: " + initial.manhattan());
+        // StdOut.println("Is goal: " + initial.isGoal());
+        Iterable<Board> iterable = initial.neighbors();
+        StdOut.println("Vecini");
+        for (Board board : iterable) {
+            StdOut.print(board.toString());
+        }
 
+        // // declare a goal 3x3 array
+        // int[][] goal3Tiles = new int[3][3];
+        // int nb = 1;
+        // for (int i = 0; i < n; i++)
+        //     for (int j = 0; j < n; j++) {
+        //         goal3Tiles[i][j] = nb;
+        //         nb++;
+        //     }
+        // goal3Tiles[2][2] = 0;
+        // Board goal3Board = new Board(goal3Tiles);
+        // StdOut.println(goal3Board.toString());
+        // StdOut.println("Is equal: " + initial.equals(goal3Board));
+
+        // // solve the puzzle
+        // Solver solver = new Solver(initial);
+        //
+        // // print solution to standard output
+        // if (!solver.isSolvable())
+        //     StdOut.println("No solution possible");
+        // else {
+        //     StdOut.println("Minimum number of moves = " + solver.moves());
+        //     for (Board board : solver.solution())
+        //         StdOut.println(board);
+        // }
     }
 
 }
